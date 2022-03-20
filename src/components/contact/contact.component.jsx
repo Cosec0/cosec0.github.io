9bc0334c808cfaf980b5a2f838886adb7f7094af
Kaushik Kumar Barman <kaushik.kb4@gmail.com>
2022-03-20 22:20:59 +0530

    added social links to contact section

--- src/components/contact/contact.component.jsx
+++ src/components/contact/contact.component.jsx
@@ -8,9 +8,9 @@ function Contact() {
             <h2>Contact</h2>
             <p>You can catch me on</p>
             <ul class="social-ul">
-            <li><a href="https://twitter.com/" target="_blank" rel="noreferrer"><i class="fab fa-twitter"></i></a></li>
-            <li><a id="profile-link" href="https://github.com/" target="_blank" rel="noreferrer"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://www.linkedin.com/" target="_blank" rel="noreferrer"><i class="fab fa-linkedin"></i></a></li>   
+            <li><a href="https://twitter.com/cosec_0" target="_blank" rel="noreferrer"><i class="fab fa-twitter"></i></a></li>
+            <li><a id="profile-link" href="https://github.com/Cosec0" target="_blank" rel="noreferrer"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://www.linkedin.com/in/kaushik-kumar-barman-23a93715b/" target="_blank" rel="noreferrer"><i class="fab fa-linkedin"></i></a></li>   
             </ul>
         </section>
     )
